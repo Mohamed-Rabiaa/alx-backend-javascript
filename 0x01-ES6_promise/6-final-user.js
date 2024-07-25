e75c9a351ba6413eb9ac997ec29eae7d0ed6c817
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-07-25 21:03:26 +0300

    Fixing code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values);
+	.then((values) => (
+	    values.map((obj) => ({
+	    status: obj.status,
+	    value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+	    }))
+	));
 }
+
