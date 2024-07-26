a291fdeaf1535e22097080ae1fe3920d80c21f26
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-07-26 23:29:35 +0300

    Adding 5-building.js file

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
+
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
   }
 
   set sqft(sqft) {
@@ -9,9 +15,5 @@ export default class Building {
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() { // eslint-disable-line class-methods-use-this
-    throw Error('Class extending Building must override evacuationWarningMessage');
   }
 }
