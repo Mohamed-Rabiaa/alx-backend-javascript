c508d728d7f8d428d00d3d02f1ff6f6e5c7f3688
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-07-31 01:29:19 +0300

    Fixing code

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,12 @@
 export default function hasValuesFromArray(set, array) {
-  return set.isSupersetOf(new Set(array));
+  let boolean = false;
+  for (const element of array) {
+    if (set.has(element)) {
+      boolean = true;
+    } else {
+      boolean = false;
+      break;
+    }
+  }
+  return boolean;
 }
