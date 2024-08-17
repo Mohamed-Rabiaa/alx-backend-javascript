7fc7d0920f4438c275a5ddb7bc33bb803a11eba4
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-08-17 18:50:54 +0300

    Add task_1/js/main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,10 @@
+// Teacher Interface
+
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any; 
+}
