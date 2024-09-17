30de4365ed065189e7fdc9b4ce1241f48361d988
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-18 01:08:49 +0300

    Fix code

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,6 +1,45 @@
 const { createServer } = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const lines = data.split('\n').filter((line) => line.trim() !== '' && line.includes(','));
+
+        let totalStudents = 0;
+        const studentsByField = {};
+        let result = '';
+
+        lines.forEach((line, index) => {
+          if (index === 0) return;
+          const [firstname, lastname, age, field] = line.split(',');
+          if (firstname && lastname && age && field) {
+            totalStudents += 1;
+            if (!studentsByField[field]) {
+              studentsByField[field] = [];
+            }
+            studentsByField[field].push(firstname);
+          }
+        });
+
+        result += `Number of students: ${totalStudents}\n`;
+        const fields = Object.entries(studentsByField);
+        fields.forEach(([field, students], index) => {
+          result += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
+
+          if (index < fields.length - 1) {
+            result += '\n';
+          }
+        });
+        resolve(result);
+      }
+    });
+  });
+}
 const hostname = '127.0.0.1';
 const port = 1245;
 
