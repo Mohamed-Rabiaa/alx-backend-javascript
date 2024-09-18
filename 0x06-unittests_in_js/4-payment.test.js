868ca5cd588db038f7352dfe6cd114e1fb945233
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-18 23:39:57 +0300

    Add 4-payment.js, 4-payment.test.js

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,20 @@
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./4-payment.js');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  it('', () => {
+    const spy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber');
+
+    stub.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    stub.calledWith('SUM', 100, 20);
+
+    expect(spy.calledWith('The total is: 10')).to.be.true;
+
+    stub.restore();
+    spy.restore();
+  });
+});
