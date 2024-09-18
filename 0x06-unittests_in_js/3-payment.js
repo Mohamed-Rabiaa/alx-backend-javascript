62b588745bd3c0cb30d86459eef9be921e356aff
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-09-18 21:52:10 +0300

    Fix code

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  return `The total is: ${total}`;
+  console.log(`The total is: ${total}`);
 }
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -6,7 +6,7 @@ const { expect } = require('chai');
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber with SUM and the correct arguments', () => {
     const spy = sinon.spy(Utils, 'calculateNumber');
-    const total = sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(100, 20);
     expect(spy.calledWith('SUM', 100, 20)).to.be.true;
     expect(spy.callCount).to.be.equal(1);
 
